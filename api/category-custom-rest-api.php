15f9d65eaf237875610f970ee24300ec3a9fd979
Andrés Venegas <andresvenegas@MacBook-Pro.local>
2021-01-06 15:30:11 -0600

    Se agrego custo field category para rest api

---
+++ api/category-custom-rest-api.php
@@ -0,0 +1,29 @@
+<?php 
+
+add_action( 'rest_api_init', 'add_custom_fields_category' );
+function add_custom_fields_category() {
+    
+
+    register_rest_field(
+        'category', 
+        'datos', //New Field Name in JSON RESPONSEs
+        array(
+            'get_callback'    => 'get_custom_fields_category', // custom function name 
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+
+    function get_custom_fields_category($object){
+
+        $color = get_term_meta( $object['id'], 'category_color', true );
+
+
+        $datos = array(
+            'color' => $color, 
+        );
+
+        return $datos;
+    }
+
+}
